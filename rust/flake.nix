387d77fc6e385a4627f94f18f87528ae49c521ed
santoshxshrestha <santoshxshrestha@gmail.com>
2025-07-20 23:52:16 +0545

    added the flake for rust

---
+++ rust/flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "A Nix-flake-based Rust development environment";
+
+  inputs = { nixpkgs.url = "github:NixOS/nixpkgs?ref=nixos-unstable"; };
+
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+    in {
+      devShells.${system}.default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          rustc
+          cargo
+          rustfmt
+          openssl
+          rust-analyzer
+          pkg-config
+        ];
+
+        shellHook = ''
+          export RUST_SRC_PATH=${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}
+        '';
+      };
+    };
+}
