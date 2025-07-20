9fc56691742a7fc697fc5e104130d0ccd808331b
santoshxshrestha <santoshxshrestha@gmail.com>
2025-07-21 00:39:02 +0545

    added the flake for C and C++

---
+++ bash/flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "A Nix-flake-based Bash development environment for bash";
+  inputs = { nixpkgs.url = "github:NixOS/nixpkgs?ref=nixos-unstable"; };
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+    in {
+      devShells.${system}.default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          bash
+          shellcheck
+          shfmt
+          bash-language-server
+          bats
+          jq
+          curl
+          git
+        ];
+        shellHook = ''
+          # Bash development environment ready
+        '';
+      };
+    };
+}
