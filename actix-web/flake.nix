cb2c73e316d73ca6468e9a2bffd435d86d54c8a0
santoshxshrestha <santoshxshrestha@gmail.com>
2025-09-18 20:00:07 +0545

    update flake

--- actix-web/flake.nix
+++ actix-web/flake.nix
@@ -1,37 +1,48 @@
 {
-  description = "A Nix-flake-based actix-web development environment";
+  description =
+    "A Nix-flake-based actix-web development environment (multi-system, flake-utils)";
 
-  inputs = { nixpkgs.url = "github:NixOS/nixpkgs?ref=nixos-unstable"; };
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    naersk.url = "github:nix-community/naersk";
+  };
 
-  outputs = { self, nixpkgs }:
-    let
-      system = "x86_64-linux";
-      # pkgs = import nixpkgs { inherit system; };
-      pkgs = nixpkgs.legacyPackages.${system};
+  outputs = { nixpkgs, flake-utils, naersk, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = (import nixpkgs) { inherit system; };
+        naerskLib = pkgs.callPackage naersk { };
+      in {
+        packages.default = naerskLib.buildPackage {
+          src = ./.;
+          buildInputs = with pkgs; [ openssl ];
+          nativeBuildInputs = [ pkgs.pkg-config ];
+        };
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            nodejs
+            rustc
+            cargo
+            clippy
+            rustfmt
+            openssl
+            rust-analyzer
 
-    in {
-      devShells.${system}.default = pkgs.mkShell {
-        buildInputs = with pkgs; [
-          nodejs
-          rustc
-          cargo
-          clippy
-          rustfmt
-          openssl
-          rust-analyzer
+            yarn
+            pnpm
+            # nodePackages.typescript
+            nodePackages.eslint
+            nodePackages.prettier
+            nodePackages.typescript-language-server
+          ];
 
-          yarn
-          pnpm
-          # nodePackages.typescript
-          nodePackages.eslint
-          nodePackages.prettier
-          nodePackages.typescript-language-server
-        ];
-        packages.${system}.default = pkgs.callPackage ./nix/default.nix { };
-        nativeBuildInputs = [ pkgs.pkg-config ];
+          formatter = pkgs.rustfmt;
 
-        env.RUST_SRC_PATH =
-          "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
-      };
-    };
+          nativeBuildInputs = [ pkgs.pkg-config ];
+
+          env.RUST_SRC_PATH =
+            "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
+        };
+      });
 }
