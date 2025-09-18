9f36f107a9ebb277931c837ace094bdc898851ef
santoshxshrestha <santoshxshrestha@gmail.com>
2025-09-18 21:20:19 +0545

    update shell

--- rust/flake.nix
+++ rust/flake.nix
@@ -4,32 +4,31 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs?ref=nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
     naersk.url = "github:nix-community/naersk";
   };
 
-  outputs = { nixpkgs, flake-utils, naersk, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs { inherit system; };
-        naerskLib = pkgs.callPackage naersk { };
-      in {
-        packages.default = naerskLib.buildPackage {
-          src = ./.;
-          buildInputs = with pkgs; [ openssl ];
-          nativeBuildInputs = [ pkgs.pkg-config ];
-        };
-        devShells.default = pkgs.mkShell {
-          buildInputs = with pkgs; [
-            rustc
-            cargo
-            clippy
-            rustfmt
-            openssl
-            rust-analyzer
-          ];
-          nativeBuildInputs = [ pkgs.pkg-config ];
-        };
-        formatter = pkgs.rustfmt;
-      });
+  outputs = { nixpkgs, naersk, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+      naerskLib = pkgs.callPackage naersk { };
+    in {
+      packages.${system}.default = naerskLib.buildPackage {
+        src = ./.;
+        buildInputs = with pkgs; [ openssl ];
+        nativeBuildInputs = [ pkgs.pkg-config ];
+      };
+      devShells.${system}.default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          rustc
+          cargo
+          clippy
+          rustfmt
+          openssl
+          rust-analyzer
+        ];
+        nativeBuildInputs = [ pkgs.pkg-config ];
+      };
+      formatter = pkgs.rustfmt;
+    };
 }
